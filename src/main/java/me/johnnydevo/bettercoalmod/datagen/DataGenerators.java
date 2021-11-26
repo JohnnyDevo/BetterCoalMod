6e18524f3ffb87b9f79b31085c7fdc3aebf15c34
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-11-26 18:23:48 -0500

    create loot tables

--- src/main/java/me/johnnydevo/bettercoalmod/datagen/DataGenerators.java
+++ src/main/java/me/johnnydevo/bettercoalmod/datagen/DataGenerators.java
@@ -1,10 +1,7 @@
 package me.johnnydevo.bettercoalmod.datagen;
 
 import me.johnnydevo.bettercoalmod.BetterCoalMod;
-import me.johnnydevo.bettercoalmod.datagen.client.ModBlockStateProvider;
-import me.johnnydevo.bettercoalmod.datagen.client.ModBlockTagsProvider;
-import me.johnnydevo.bettercoalmod.datagen.client.ModItemModelProvider;
-import me.johnnydevo.bettercoalmod.datagen.client.ModItemTagsProvider;
+import me.johnnydevo.bettercoalmod.datagen.client.*;
 import net.minecraft.data.*;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,18 +14,16 @@ public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
-        if (event.includeServer()) {
-//            generator.addProvider(new RecipeProvider(generator));
-//            generator.addProvider(new LootTableProvider(generator));
-        }
-        if (event.includeClient()) {
-            ExistingFileHelper helper = event.getExistingFileHelper();
-            generator.addProvider(new ModBlockStateProvider(generator, helper));
-            generator.addProvider(new ModItemModelProvider(generator, helper));
 
-            ModBlockTagsProvider blockTags = new ModBlockTagsProvider(generator, helper);
-            generator.addProvider(blockTags);
-            generator.addProvider(new ModItemTagsProvider(generator, blockTags, helper));
-        }
+        ExistingFileHelper helper = event.getExistingFileHelper();
+        generator.addProvider(new ModBlockStateProvider(generator, helper));
+        generator.addProvider(new ModItemModelProvider(generator, helper));
+
+        ModBlockTagsProvider blockTags = new ModBlockTagsProvider(generator, helper);
+        generator.addProvider(blockTags);
+        generator.addProvider(new ModItemTagsProvider(generator, blockTags, helper));
+
+        generator.addProvider(new ModLootTableProvider(generator));
+
     }
 }
---
+++ src/main/java/me/johnnydevo/bettercoalmod/datagen/client/ModLootTableProvider.java
@@ -0,0 +1,54 @@
+package me.johnnydevo.bettercoalmod.datagen.client;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import me.johnnydevo.bettercoalmod.setup.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.LootTableProvider;
+import net.minecraft.data.loot.BlockLootTables;
+import net.minecraft.loot.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.RegistryObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ModLootTableProvider extends LootTableProvider {
+    public ModLootTableProvider(DataGenerator pGenerator) {
+        super(pGenerator);
+    }
+
+    @Override
+    protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
+        return ImmutableList.of(
+                Pair.of(ModBlockLootTables::new, LootParameterSets.BLOCK)
+        );
+    }
+
+    @Override
+    protected void validate(Map<ResourceLocation, LootTable> map, ValidationTracker validationTracker) {
+        map.forEach((param1, param2) -> LootTableManager.validate(validationTracker, param1, param2));
+    }
+
+    public static class ModBlockLootTables extends BlockLootTables {
+        @Override
+        protected void addTables() {
+            dropSelf(ModBlocks.COMPRESSED_COAL_BLOCK.get());
+            dropSelf(ModBlocks.MATTER_COMPRESSOR.get());
+            dropSelf(ModBlocks.MATTER_DECOMPRESSOR.get());
+            dropSelf(ModBlocks.MATTER_RECOMPRESSOR.get());
+        }
+
+        @Override
+        protected Iterable<Block> getKnownBlocks() {
+            return ModBlocks.BLOCKS.getEntries().stream()
+                    .map(RegistryObject::get)
+                    .collect(Collectors.toList());
+        }
+    }
+}
